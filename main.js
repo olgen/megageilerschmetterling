b83d11e99e80c89d7c4591b6bc31dbf7da3f2b88
Eugen Martin <eugeniusmartinus@gmail.com>
2023-06-27 04:58:20 +0200

    add more effects - accel reaction mode - eyes and antennae - kudos to Natalie

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ var accelerationToModeSwitch = 0.03;
 var firstWingsPixel = 0;
 var lastWingsPixel = 225;
 var firstEyesPixel = 226;
-var lastEyesPixel = 234;
-var firstAntennaPixel = 235;
-var lastAntennaPixel = 236;
+var lastEyesPixel = 233;
+var firstAntennaPixel = 234;
+var lastAntennaPixel = 235;
 
 historyLength = 50;
 var accelHistory = array(historyLength);
@@ -38,20 +38,22 @@ export var h = 1;
 export var s = 1;
 export var v = 1;
 
+var va;
+
 var lastBlink = 0;
 export var debug = 0;
 
 modes = [
   effectSpin,
   (index) => {
-    h = 0.5;
+    // h = 0.5;
     v = brightness;
   },
   (index) => {
     if (buttonValue == 0.0) {
       lastBlink = now;
     }
-    h = 0.7;
+    // h = 0.7;
     v = brightness;
     s = 1;
 
@@ -61,7 +63,7 @@ modes = [
       if (delta == 1) {
         lastBlink = 0;
       }
-      h = max(0.7, 1 - delta);
+      // h = max(0.7, 1- delta)
       v = max(brightness, 1 - delta);
       s = min(1, delta);
     }
@@ -100,15 +102,19 @@ export function gaugeVerticalAccel() {
   return normalizedVA;
 }
 
-function updateAccelHistory(delta) {
-  va = accelerometer[1];
-  accelHistory[currentIdx] = va;
-  currentIdx += 1;
-  if (currentIdx == accelHistory.length) {
-    currentIdx = 0;
-  }
-  averageVA = arraySum(accelHistory) / arrayLength(accelHistory);
-  normalizedVA = +0.5 + averageVA * 10; // use 0 as middle for the slider
+function updateAccelerometer(delta) {
+  // ground: -0.02
+  // range: -0.005 - -0.035
+  newValue = accelerometer[1];
+  va = 0.95 * va + 0.05 * newValue;
+
+  // accelHistory[currentIdx] = va;
+  // currentIdx += 1;
+  // if (currentIdx == accelHistory.length) {
+  //   currentIdx = 0;
+  // }
+  // averageVA = arraySum(accelHistory) / arrayLength(accelHistory);
+  // normalizedVA = +0.5 + averageVA * 10; // use 0 as middle for the slider
 }
 
 function mapVAToZeroToOne(t) {
@@ -131,6 +137,10 @@ function setBrightness() {
   brightness = brightness * (1 - weight) + newValue * weight;
 }
 
+function setHue() {
+  h = 1 + va * 8;
+}
+
 function updatePot() {
   newValue = analogInputs[4];
   pot = pot * (1 - weight) + newValue * weight;
@@ -140,38 +150,77 @@ function updateButton() {
   buttonValue = digitalRead(BUTTON_PIN);
 }
 
-function renderEye(index) {
-  if (index == firstEyesPixel) {
-    hsv(0.99, 1, wave(time(0.07)));
+function renderWing(index) {
+  modes[mode](index);
+  if (index < 50 + 5000 * (va + 0.02)) {
+    v = brightness;
+  } else if (index > lastWingsPixel - 48 - 5000 * (va + 0.02)) {
+    v = brightness;
   } else {
-    hsv(0.89, 1, wave(time(0.1)));
+    v = 0;
   }
+  hsv(h, s, v);
+}
+
+// i broke this again with making the vars local but eh
+function renderEye(index) {
+  pixelRange = lastEyesPixel - firstEyesPixel;
+  pct = index / pixelRange; // Percent this pixel is into the eyes part of the strip
+  hh = pct * (5 * wave(t1 * 3) + 5) + 2 * wave(t1 * 2);
+  hh = (h % 0.5) + t1; // Remainder has precedence over addition
+  vv = triangle(5 * pct + 10 * t1);
+  vv = vv * vv * vv;
+  vv = max(brightness, vv);
+  hsv(hh, 1, vv);
 }
 
 function renderAntenna(index) {
-  if (index == firstAntennaPixel) {
-    hsv(0.95, 1, wave(time(0.07)));
-  } else {
-    hsv(0.96, 1, wave(time(0.1)));
+  if (buttonValue == 0.0) {
+    lastBlink = now;
   }
+  hh = 0.7;
+  vv = brightness;
+  ss = 1;
+
+  if (lastBlink != 0) {
+    delta = min(now - lastBlink, 500) / 500.0;
+    debug = delta / 4;
+    if (delta == 1) {
+      lastBlink = 0;
+    }
+    hh = max(0.7, 1 - delta);
+    vv = max(brightness, 1 - delta);
+    ss = min(1, delta);
+  }
+
+  hsv(hh, ss, vv);
+
+  // var hh = wave(time(0.2))
+  // if (index == firstAntennaPixel) {
+  //   hsv(hh,0.7,wave(time(0.06)))
+  // } else {
+  //   hsv(hh,0.9,wave(time(0.1)))
+  // }
 }
 
 export function beforeRender(delta) {
   now += delta;
   t1 = time(0.1);
   checkModeSwitch(delta);
-  updateAccelHistory(delta);
+  updateAccelerometer(delta);
   //setSaturation()
   setBrightness();
+  setHue();
   updatePot();
   updateButton();
+  if (va > -0.015) {
+    buttonValue = 0;
+  }
 }
 
 export function render(index) {
   if (index <= lastWingsPixel) {
-    // render body
-    modes[mode](index);
-    hsv(h, s, v);
+    renderWing(index);
   } else if (index <= lastEyesPixel) {
     renderEye(index);
   } else {
